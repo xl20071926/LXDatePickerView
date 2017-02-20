86fd80f0986bb58eba38289dada48afff8def5fb
xl20071926 <lixin_1990@yeah.net>
2017-02-20 10:42:43 +0800

    add missing file

---
+++ LXDatePickerView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LXDatePickerView
+//
+//  Created by Leexin on 17/2/18.
+//  Copyright © 2017年 Garden.Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
